3a88636ea9d648d134bb6013ea6267d06c30a77c
hex-color <36766568+hex-color@users.noreply.github.com>
2018-05-11 04:19:05 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -108,7 +108,10 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "0000000000000000
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "18.219.35.33:48734",
-  "18.188.214.134:48734",
+  "13.58.156.253:48734",
+  "52.14.51.48:48734",
+  "13.59.38.129:48734",
+  "52.15.57.88:48734",
 };
 
 struct CheckpointData {
