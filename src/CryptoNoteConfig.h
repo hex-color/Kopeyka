00a3af8f827aba19a3a75ad39036ed08a2840da8
hex-color <36766568+hex-color@users.noreply.github.com>
2018-05-11 04:03:41 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -24,7 +24,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO Specify total number of available coins
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(240190070400000000000);
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(24019097004);
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 18 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
@@ -33,7 +33,7 @@ const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 100;
 const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 10000; //size of block (bytes) after which reward for block calculated using block size
 const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE        = 600;
 //TODO Define number of digits
-const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 8;
+const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 16;
 //TODO Define minimum fee for transactions
 const uint64_t MINIMUM_FEE                                   = 103020;
 const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
