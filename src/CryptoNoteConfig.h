af83ebd2a94c8dd241c486d28459ebc0c205cba7
hex-color <36766568+hex-color@users.noreply.github.com>
2018-05-11 23:47:42 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -75,7 +75,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Kopeyka";
 const char     GENESIS_COINBASE_TX_HEX[]                     = 
-"";
+"013c01ff0001e9cb05029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121018985ba4465d2ecf3bf40c002ec7c4971dd0f373db931932a6204998f3268ef07";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
